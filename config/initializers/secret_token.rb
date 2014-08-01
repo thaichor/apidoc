56499b9f8a7b28b1503c93cd4fe1121deee742ce
Thaichor Seng <thaichor@gmail.com>
2014-08-01 10:20:28 +0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Apidoc::Application.config.secret_key_base = '15f7c720c525698eef84530e7d2635264d7a8e7debf2b3eb8b3c5228a2e93924f50c0a4c1573166f84f5b669895aed0ed02003072178a4691361f9eca705c73b'
